480b65964567e535439ea833c003875a68722bc6
William11Cassidy <121349385+William11Cassidy@users.noreply.github.com>
2023-09-04 14:43:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 function preload(){
 mustache=loadImage("mustache.avif")
+
 }
 
 
 function   setup(){
-canvas=createCanvas(300,300)
-canvas.center()
+canvas = createCanvas(300, 300)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(300, 300)
+    video.hide()
+
+    poseNet = ml5.poseNet(video, modelLoaded)
+    poseNet.on("pose", gotResult)
 }
 
 
@@ -13,5 +20,18 @@ function  draw(){
 image(mustache,50,50,100,70)
 }
 function takesnapshot(){
-    save("clownfilter.png")
+    save("mustachefilter.png")
+}
+
+function modelLoaded() {
+    console.log("poseNet is loaded");
+}
+
+
+function gotResult(result) {
+    if (result.length>0) {
+
+        console.log(result);
+        console.log("x= " + result[0].pose.nose.x + " \n y= " + result[0].pose.nose.y);
+    }
 }
